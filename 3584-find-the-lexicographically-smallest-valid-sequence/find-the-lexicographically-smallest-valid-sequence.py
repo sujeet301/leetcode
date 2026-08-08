e76159984678550d0c8fc8e855797256f034d570
sujeet301 <sujeetwebdev@gmail.com>
2026-08-08 23:04:13 +0530

    Time: 760 ms (85.71%) | Memory: 51.4 MB (57.14%) - LeetSync

---
+++ 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def validSequence(self, word1, word2):
+        n = len(word1)
+        m = len(word2)
+        last = [-1] * m
+        i = n - 1
+        j = m - 1
+
+        while i >= 0 and j >= 0:
+            if word1[i] == word2[j]:
+                last[j] = i
+                j -= 1
+            i -= 1
+
+        ans = []
+        j = 0
+        changed = False
+
+        for i in range(n):
+            if j == m:
+                break
+
+            if word1[i] == word2[j]:
+                ans.append(i)
+                j += 1
+
+            elif not changed and (
+                j == m - 1 or i < last[j + 1]
+            ):
+                ans.append(i)
+                j += 1
+                changed = True
+
+        if len(ans) == m:
+            return ans
+
+        return []
