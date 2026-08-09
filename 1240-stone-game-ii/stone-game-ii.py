91e49443432d7864760b22a5bcbcc4e2d790cb3f
sujeet301 <sujeetwebdev@gmail.com>
2026-08-09 17:47:18 +0530

    Time: 320 ms (36.46%) | Memory: 14.9 MB (21.87%) - LeetSync

---
+++ 1240-stone-game-ii/stone-game-ii.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def stoneGameII(self, piles):
+        n = len(piles)
+
+        suffix = [0] * (n + 1)
+
+        for i in range(n - 1, -1, -1):
+            suffix[i] = suffix[i + 1] + piles[i]
+
+        memo = {}
+
+        def dfs(i, M):
+            if i >= n:
+                return 0
+
+            if (i, M) in memo:
+                return memo[(i, M)]
+
+            best = 0
+
+            for X in range(1, 2 * M + 1):
+                if i + X > n:
+                    break
+
+                opponent = dfs(i + X, max(M, X))
+
+                current = suffix[i] - opponent
+
+                best = max(best, current)
+
+            memo[(i, M)] = best
+            return best
+
+        return dfs(0,1)
