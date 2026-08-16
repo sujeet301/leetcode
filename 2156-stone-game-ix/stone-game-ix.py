cc2ef74317197cd8f567a7398e1e5f177629e6fa
sujeet301 <sujeetwebdev@gmail.com>
2026-08-16 10:11:19 +0530

    Time: 58 ms (57.14%) | Memory: 20.3 MB (0.00%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def stoneGameIX(self, stones):
+        count = [0, 0, 0]
+
+        for stone in stones:
+            count[stone % 3] += 1
+
+        if count[0] % 2 == 0:
+            return count[1] > 0 and count[2] > 0
+
+        return abs(count[1] - count[2]) > 2
