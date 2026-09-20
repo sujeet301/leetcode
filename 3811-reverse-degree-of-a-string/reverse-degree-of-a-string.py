a56986f6b07798df3a7cf5b9b0c1f3385c65c51f
sujeet301 <sujeetwebdev@gmail.com>
2026-09-20 18:01:45 +0530

    Time: 10 ms (64.94%) | Memory: 12.3 MB (89.67%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def reverseDegree(self, s):
+        answer = 0
+
+        for i in range(len(s)):
+            reverse_value = 26 - (ord(s[i]) - ord('a'))
+            position = i + 1
+
+            answer += reverse_value * position
+
+        return answer
